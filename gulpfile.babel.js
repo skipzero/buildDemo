3e906fe6d0d96d43ff2af55aed285515de06fa5a
zerosquadron <skip@zerosquadron.com>
2016-05-01 18:27:33 -0700

    bower and gulp work

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,40 +1,64 @@
 'use strict';
 
 const gulp 				= require('gulp');
-const gulputil 		= require('gulp-util');
+const gutil 			= require('gulp-util');
+const bower 			= require('gulp-bower');
 const sass 				= require('gulp-sass');
+const notify 			= require('gulp-notify');
 const runSequence = require('run-sequence');
-const uglify 			= require('gulp-uglify');
 const watch 			= require('gulp-watch');
 const del 				= require('del');
 
+const config = {
+	sass: './src/css',
+	js: 	'./src/js',
+	bower: './bower_components',
+	bowerCss: './bower_components/bootstrap-sass/assets/stylesheets',
+	bowerJquery: './bower_components/jquery',
+	html: './src/**/*.html'
+
+}
+
 
 gulp.task('watch', () => {
-	watch(['src/**/*.scss', 'src/**/*.js', 'src/**/*.html'], () => {
+	watch([config.sass + '/*.scss', config.js + '/*.js', 'src/**/*.html'], () => {
 		gulp.start();
 	})
 });
 
-gulp.task('default', () => { runSequence(['clean'], ['build'], ['sass'], ['js'] )});
+gulp.task('default', () => { runSequence(['clean'], ['bower'], ['build'], ['sass'], ['js'] )});
 
 // move our templates and/or static files
 gulp.task('build', () => {
-	return gulp.src(['./src/**/*.html'])
+	return gulp.src([config.html])
 	.pipe(gulp.dest('./build/'));
 });
 
+gulp.task('bower', () => {
+	return bower().pipe(
+		gulp.dest(config.bower)) 
+})
+
 // processes and moves our stylesheets.
 gulp.task('sass', () => {
-	return gulp.src(['./src/**/*.scss', './src/vendor/**.*.scss'])
-	.pipe(sass().on('error', sass.logError))
-	.pipe(gulp.dest('./build/'));
+	return gulp.src([config.sass + '/*.scss', './src/vendor/**.*.scss'])
+	.pipe(sass({
+		style: 'compressed',
+		loadPath: [
+			config.sass,
+			config.bowerCss + '/**/*.scss'
+		 ]
+	}).on('error', notify.onError( (error) => {
+		return 'ERROR: ' + error.message;
+	})))
+	.pipe(gulp.dest('./build/css'));
 });
 
 // uglify our JS and move to build
 gulp.task('js', () => {
-	return gulp.src(['./src/**/*.js'])
+	return gulp.src([config.js + '/*.js'])
 	// .pipe(uglify())
-	.pipe(gulp.dest('./build/'))
+	.pipe(gulp.dest('./build/js'))
 })
 
 gulp.task('clean', () => {
